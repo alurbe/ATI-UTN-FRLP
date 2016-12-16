e641f7f0864b197ccc25401b4db713f70a1724cd
alurbe <alelurbe@gmail.com>
2016-12-16 16:46:43 -0300

    Aplicación web biblioteca
    Trabajo practico materia ATI

---
+++ BibliotecaWeb/BibliotecaWeb/Models/EmpModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BibliotecaWeb.Models
+{
+    public class EmpModel
+    {
+        [Display(Name="Birth name")]
+        [DataType(DataType.Date)]
+
+        public DateTime? DOB { get; set; }
+    }
+}
